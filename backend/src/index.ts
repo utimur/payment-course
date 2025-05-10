10f410efb855a6645372abce7f6723b5d0528de0
utimur <akva_ten@mail.ru>
2025-05-10 17:10:07 +0300

    initial project

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = process.env.PORT || 4001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+    res.json({ message: 'test' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div>
+      INITIAL PROJECT
+    </div>
+  )
+}
+
+export default App
