7e6c4b8b2f642ed83601d5f3f9f650057c53ea2d
utimur <akva_ten@mail.ru>
2025-05-11 15:59:35 +0300

    add payment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,10 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import {ICreatePayment, YooCheckout} from '@a2seven/yoo-checkout';
+
+const database: any = {
+
+}
 
 const app = express();
 const PORT = process.env.PORT || 4001;
@@ -7,9 +12,53 @@ const PORT = process.env.PORT || 4001;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-    res.json({ message: 'test' });
+// ---------------------------------
+
+const YouKassa = new YooCheckout({
+    shopId: '1082575',
+    secretKey: 'test_eoCC7M0gupEbxLPkH2JEr6FyhccP_T_LatFk-0hxvgc'
 });
+
+app.post('/api/payment', async (req: Request, res: Response) => {
+    const createPayload: ICreatePayment = {
+        amount: {
+            value: req.body.value,
+            currency: 'RUB'
+        },
+        payment_method_data: {
+            type: 'bank_card'
+        },
+        capture: true,
+        confirmation: {
+            type: 'redirect',
+            return_url: 'https://secondly-worthy-shrimp.cloudpub.ru'
+        },
+        metadata: {
+            orderId: req.body.orderId,
+            userId: req.body.userId,
+        }
+    };
+
+    try {
+        const payment = await YouKassa.createPayment(
+            createPayload,
+            // Не делать так в проде
+            Date.now().toString()
+        );
+
+        database[payment.id] = payment;
+        res.json({payment})
+    } catch (error) {
+        console.error(error);
+        res.status(400).json({error: 'error'});
+    }
+});
+
+app.post('/api/payment/notifications', async (req: Request, res: Response) => {
+    console.log(req.body)
+    database[req.body.id] = req.body;
+    res.json({status: "OK"})
+})
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,27 @@
 function App() {
 
+    const onClick = () => {
+        fetch('https://ravishingly-potential-hornbill.cloudpub.ru/api/payment', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                value: '10.00',
+                orderId: 'orderId',
+                userId: 'userId',
+            }),
+        }).then(res => res.json())
+            .then(data => {
+                // navigate()
+                console.log(data)
+                window.open(data?.payment?.confirmation?.confirmation_url)
+            })
+    }
+
   return (
     <div>
-      INITIAL PROJECT
+      <button onClick={onClick}>ОПЛАТИТЬ!!!!</button>
     </div>
   )
 }
